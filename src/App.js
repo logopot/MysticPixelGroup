639bf0cce83823a404eb9b2b83acbba1310a0e9e
Logopot <logopotposao@gmail.com>
2024-01-09 01:08:30 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Router from "./Router";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import { Container } from "react-bootstrap";
-import Nav from "./components/Nav/Nav";
+// import Nav from "./components/Nav/Nav";
 
 const App = () => {
   return (
