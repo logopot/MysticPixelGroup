927bd360071e70879a3eb79e411ded507c7a03bb
Logopot <logopotposao@gmail.com>
2024-01-11 00:37:14 +0100

    creating main title component

---
+++ src/components/MainTitle/MainTitle.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { StyledMainTitle } from "./MainTitle.styled";
+
+const MainTitle = ({ title }) => {
+  return <StyledMainTitle>{title}</StyledMainTitle>;
+};
+
+export default MainTitle;
---
+++ src/components/MainTitle/MainTitle.styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const StyledMainTitle = styled.h1`
+  font-size: 100px;
+  max-width: 600px;
+  margin: 100px 0 150px;
+  font-weight: 600;
+  color: ${(props) => props.theme.textColor};
+`;
