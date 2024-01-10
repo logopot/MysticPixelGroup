928714499bccf599358be4a0794594a014995e84
Logopot <logopotposao@gmail.com>
2024-01-11 00:54:52 +0100

    Update MainTitle.styled.js

--- src/components/MainTitle/MainTitle.styled.js
+++ src/components/MainTitle/MainTitle.styled.js
@@ -1,9 +1,14 @@
 import styled from "styled-components";
 
 export const StyledMainTitle = styled.h1`
-  font-size: 100px;
-  max-width: 600px;
-  margin: 100px 0 150px;
+  font-size: 60px;
+  margin: 50px 0 70px;
   font-weight: 600;
   color: ${(props) => props.theme.textColor};
+
+  @media (min-width: 769px) {
+    font-size: 90px;
+    max-width: 770px;
+    margin: 100px 0 150px;
+  }
 `;
