31ecbe22e4cafb960fe6ae507d6e1aaca10daca6
Logopot <logopotposao@gmail.com>
2024-01-11 00:36:53 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,25 @@ import Router from "./Router";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import { Container } from "react-bootstrap";
-// import Nav from "./components/Nav/Nav";
+
+import background from "./assets/body-bg/body-bg.jpg";
+import Nav from "./components/Nav/Nav";
 
 const App = () => {
   return (
     <>
-      <Container>
-        {/* <Nav /> */}
-        <Router />
-      </Container>
+      <div
+        className="body"
+        style={{
+          backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 1)), url(${background})`,
+        }}
+      >
+        <div className="vertical-line"></div>
+        <Container fluid>
+          <Nav />
+          <Router />
+        </Container>
+      </div>
     </>
   );
 };
