7656da5325e0ce709d7727c9e5f2813d239244da
Logopot <logopotposao@gmail.com>
2024-01-12 00:37:45 +0100

    separator

---
+++ src/components/Separator/Separator.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { SeparatorDiv } from "./Separator.styled";
+
+const Separator = ({ marginTop }) => {
+  return <SeparatorDiv marginTop={marginTop} />;
+};
+
+export default Separator;
---
+++ src/components/Separator/Separator.styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const SeparatorDiv = styled.div`
+  height: 1px;
+  margin-top: 80px;
+
+  @media (min-width: 768px) {
+    margin-top: 150px;
+  }
+`;
