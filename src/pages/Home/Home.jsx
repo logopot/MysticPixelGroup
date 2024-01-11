76bce53ea489b07a859cc0da019a26f083bf9ace
Logopot <logopotposao@gmail.com>
2024-01-12 00:38:15 +0100

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,20 @@
 import { Col, Row } from "react-bootstrap/esm";
 import MainTitle from "../../components/MainTitle/MainTitle";
+import ProductCard from "../../components/ProductCard/ProductCard";
 const Home = () => {
   return (
-    <Row>
-      <Col md={{ span: 8, offset: 2 }}>
-        <MainTitle title="Digital design and development agency" />
-      </Col>
-    </Row>
+    <>
+      <Row>
+        <Col>
+          <MainTitle title="Game design and development agency" />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <ProductCard />
+        </Col>
+      </Row>
+    </>
   );
 };
 
