d51da4d11be0bbec968ed2104b4faa7d9373a553
Logopot <logopotposao@gmail.com>
2024-01-14 13:33:29 +0100

    fix

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,6 +1,5 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
-import Home2 from "./pages/Home2/Home2";
 
 import ScrollToTop from "./ScrollToTop";
 import ProductSinglePage from "./pages/ProductSinglePage/ProductSinglePage";
--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -46,7 +46,7 @@ const ProductCard = () => {
       <Row className="align-items-end">
         <Col className="pe-md-5 pt-5 pt-md-0 order-2 order-md-1">
           <StyledCathegoryTag>Platform</StyledCathegoryTag>
-          <StyledTitleLink href="/">
+          <StyledTitleLink href="/MysticPixelGroup/MysticPixel">
             <StyledTitle>Pixel Quest</StyledTitle>
           </StyledTitleLink>
           <StyledDescription>
@@ -55,10 +55,13 @@ const ProductCard = () => {
             abilities, and face challenging bosses as you uncover the mystery
             behind the mystic pixel group.
           </StyledDescription>
-          <LinkLabel url="/MysticPixelGroup" label="Explore the details" />
+          <LinkLabel
+            url="/MysticPixelGroup/MysticPixel"
+            label="Explore the details"
+          />
         </Col>
         <Col md={6} className="ps-md-5 order-1 order-md-2">
-          <a href="/">
+          <a href="/MysticPixelGroup/MysticPixel">
             <StyledImage className="img-fluid" src={image2} alt="product" />
           </a>
         </Col>
@@ -68,13 +71,13 @@ const ProductCard = () => {
 
       <Row>
         <Col lg={12} xl={7} className="pe-xl-5">
-          <a href="/">
+          <a href="/MysticPixelGroup/MysticPixel">
             <StyledImage className="img-fluid" src={image3} alt="product" />
           </a>
         </Col>
         <Col lg={12} xl={5} className="pt-5 pt-xl-0">
           <StyledCathegoryTag>Shooter</StyledCathegoryTag>
-          <StyledTitleLink href="/">
+          <StyledTitleLink href="/MysticPixelGroup/MysticPixel">
             <StyledTitle>Pixel Blast: The Final Countdown</StyledTitle>
           </StyledTitleLink>
           <StyledDescription>
@@ -84,7 +87,7 @@ const ProductCard = () => {
             bonuses. The pixel blast is getting closer every second, so you have
             to hurry and reach the safe zone before it’s too late.
           </StyledDescription>
-          <LinkLabel url="/MysticPixelGroup" label="Go beyond" />
+          <LinkLabel url="/MysticPixelGroup/MysticPixel" label="Go beyond" />
         </Col>
       </Row>
 
